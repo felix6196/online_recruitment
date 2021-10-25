f864661132d3c77a41e827280fc1cc93e252c66d
felix6196 <52097047+felix6196@users.noreply.github.com>
2021-10-25 15:54:03 +0530

    Update __manifest__.py

--- inherit_hr_recruitment/__manifest__.py
+++ inherit_hr_recruitment/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-{   'name' : 'Online recruitment',
+{   'name' : 'Smart Job Application Form',
     'version' : '11.0.1.0.0',
     'summary': 'It use to make recruiting processes more efficient and less expensive.',
     'sequence': -1,
